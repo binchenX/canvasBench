36ee615aea76a558424e8f99190706f9a8604f72
binchen1 <bin.chen@intel.com>
2013-03-19 00:52:04 +0800

    add draw Shader

--- src/com/intel/canvasbench/TestShader.java
+++ src/com/intel/canvasbench/TestShader.java
@@ -10,7 +10,6 @@ import android.graphics.SweepGradient;
 import android.view.KeyEvent;
 import android.view.View;
 
-
 public class TestShader extends AbstractTestCase {
 
 	private SampleView mTestView = null;
@@ -55,73 +54,61 @@ public class TestShader extends AbstractTestCase {
 		return mTestView;
 	}
 
-	
-	 private static class SampleView extends AstractView {
-	        private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-	        private float mRotate;
-	        private Matrix mMatrix = new Matrix();
-	        private Shader mShader;
-	        private boolean mDoTiming;
+	private static class SampleView extends AstractView {
+		private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		private float mRotate;
+		private Matrix mMatrix = new Matrix();
+		private Shader mShader;
+		private boolean mDoTiming;
 
-	        public SampleView(Context context) {
-	            super(context);
-	            setFocusable(true);
-	            setFocusableInTouchMode(true);
+		public SampleView(Context context) {
+			super(context);
+			setFocusable(true);
+			setFocusableInTouchMode(true);
 
-	            float x = 160;
-	            float y = 100;
-	            mShader = new SweepGradient(x, y, new int[] { Color.GREEN,
-	                                                  Color.RED,
-	                                                  Color.BLUE,
-	                                                  Color.GREEN }, null);
-	            mPaint.setShader(mShader);
-	        }
+			float x = 160;
+			float y = 100;
+			mShader = new SweepGradient(x, y, new int[] { Color.GREEN,
+					Color.RED, Color.BLUE, Color.GREEN }, null);
+			mPaint.setShader(mShader);
+		}
 
-
-	        @Override public boolean onKeyDown(int keyCode, KeyEvent event) {
-	            switch (keyCode) {
-	                case KeyEvent.KEYCODE_D:
-	                    mPaint.setDither(!mPaint.isDither());
-	                    invalidate();
-	                    return true;
-	                case KeyEvent.KEYCODE_T:
-	                    mDoTiming = !mDoTiming;
-	                    invalidate();
-	                    return true;
-	            }
-	            return super.onKeyDown(keyCode, event);
-	        }
-
-			@Override
-			void doDraw(Canvas canvas) {
-			
-			    Paint paint = mPaint;
-	            float x = 160;
-	            float y = 100;
-
-	            canvas.drawColor(Color.WHITE);
-
-	            mMatrix.setRotate(mRotate, x, y);
-	            mShader.setLocalMatrix(mMatrix);
-	            mRotate += 3;
-	            if (mRotate >= 360) {
-	                mRotate = 0;
-	            }
-	            invalidate();
-
-	            if (mDoTiming) {
-	                long now = System.currentTimeMillis();
-	                for (int i = 0; i < 20; i++) {
-	                    canvas.drawCircle(x, y, 80, paint);
-	                }
-	                now = System.currentTimeMillis() - now;
-	                android.util.Log.d("skia", "sweep ms = " + (now/20.));
-	            }
-	            else {
-	                canvas.drawCircle(x, y, 80, paint);
-	            }
-
+		@Override
+		public boolean onKeyDown(int keyCode, KeyEvent event) {
+			switch (keyCode) {
+			case KeyEvent.KEYCODE_D:
+				mPaint.setDither(!mPaint.isDither());
+				invalidate();
+				return true;
+			case KeyEvent.KEYCODE_T:
+				mDoTiming = !mDoTiming;
+				invalidate();
+				return true;
 			}
-	    }
-	
+			return super.onKeyDown(keyCode, event);
+		}
+
+		@Override
+		void doDraw(Canvas canvas) {
+
+			Paint paint = mPaint;
+			float x = 160;
+			float y = 100;
+			
+			canvas.drawColor(Color.WHITE);
+
+			for (int i = 0; i < 3; i++) {
+							mMatrix.setRotate(mRotate, x, y);
+				mShader.setLocalMatrix(mMatrix);
+				mRotate += 3;
+				if (mRotate >= 360) {
+					mRotate = 0;
+				}
+				canvas.drawCircle(x, y, 80, paint);
+				canvas.translate(0, 160);
+			}
+
+		}
+	}
+
 }
